fb562b6fc2a15242171a36114d00429fe2d86c75
Vell <lovell.m@teachingstrategies.com>
2016-10-04 09:07:40 -0400

    fix file generation location for setup

--- lib/my_todo/setup.rb
+++ lib/my_todo/setup.rb
@@ -6,7 +6,7 @@ require 'thor'
 class Setup < Thor
   include Thor::Actions
   # Look into the following location for templates
-  source_root "#{__dir__}/../lib/my_todo/templates"
+  source_root "#{__dir__}/../../lib/my_todo/templates"
   # Store the root of the gem directory
   GEM_DIR = "#{__dir__}/.."
   # Store users home directory
@@ -18,11 +18,11 @@ class Setup < Thor
       `mkdir -p #{HOME_DIR}/.my_todo/data`
       say "Created .my_todo in #{HOME_DIR}"
     end
-    template "config.yml.erb", "#{__dir__}/../lib/db/config.yml", force: true
+    template "config.yml.erb", "#{__dir__}/../../lib/db/config.yml", force: true
   end
 
   desc 'standard_migrations_override', 'Generate SM override file'
   def standard_migrations_override
-    template "standalone_migrations.yml.erb", "#{__dir__}/../.standalone_migrations", force: true
+    template "standalone_migrations.yml.erb", "#{__dir__}/../../.standalone_migrations", force: true
   end
 end
