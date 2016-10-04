2b2bd7646239c74487d72596af7b4c2283770095
Vell <lovell.m@teachingstrategies.com>
2016-10-04 08:56:14 -0400

    remove unecessary migrations

--- lib/db/migrate/20161003182100_create_lists.rb
+++
@@ -1,9 +0,0 @@
-class CreateLists < ActiveRecord::Migration[5.0]
-  def change
-    create_table :lists do |t|
-      t.string :name
-      t.string :value
-      t.timestamps null: false
-    end
-  end
-end
