1c004d78d57bafb0919a7011ebe40a477b16bce1
Vell <lovell.m@teachingstrategies.com>
2016-10-07 07:58:45 -0400

    update spec output

--- spec/actions/search_spec.rb
+++ spec/actions/search_spec.rb
@@ -12,18 +12,6 @@ describe MyTodo do
       end
 
 
-<<<<<<< HEAD
-      it 'returns todo item by body' do
-        expect{MyTodo::Todo.start( %w[search nfl])}.to output("ToDos FOUND: 1\n\nID: 1 | Tags: tag1 | Complete: \nnfl\n\n").to_stdout
-      end
-
-      it 'returns todo items by associated tag' do
-        expect{MyTodo::Todo.start( %w[search tag1])}.to output("ToDos FOUND: 1\n\nID: 1 | Tags: tag1 | Complete: \nnfl\n\n").to_stdout
-      end
-
-      it 'returns todo items by associated notes content' do
-        expect{MyTodo::Todo.start( %w[search note1])}.to output("ToDos FOUND: 1\n\nID: 3 | Tags:  | Complete: \nalways\nNotes:\n1: note1\n\n\n").to_stdout
-=======
       it 'finds todo item by body' do
         expect{MyTodo::Todo.start( %w[search nfl])}.to output("ToDos FOUND: 1\n\nID: 1 | Created On: 2016-10-07 | Tags: tag1 | Status:  | Complete: \nnfl\n\n").to_stdout
       end
@@ -34,13 +22,12 @@ describe MyTodo do
 
       it 'finds todo items by associated notes content' do
         expect{MyTodo::Todo.start( %w[search note1])}.to output("ToDos FOUND: 1\n\nID: 3 | Created On: 2016-10-07 | Tags:  | Status:  | Complete: \nalways\nNotes:\nID: 1 | Created On: 2016-10-07\nnote1\n\n\n").to_stdout
->>>>>>> feature/detailed-status
       end
 
       it 'returns only one todo item if criteria matches associated records' do
         @todo2.notes.create(body: 'who rocks')
         @todo2.tags.create(name: 'rocky')
-        expect{MyTodo::Todo.start( %w[search rock])}.to output("ToDos FOUND: 1\n\nID: 2 | Tags: rocky | Complete: \nrocks\nNotes:\n2: who rocks\n\n\n").to_stdout
+        expect{MyTodo::Todo.start( %w[search rock])}.to output("ToDos FOUND: 1\n\nID: 2 | Created On: 2016-10-07 | Tags: rocky | Status:  | Complete: \nrocks\nNotes:\nID: 2 | Created On: 2016-10-07\nwho rocks\n\n\n").to_stdout
       end
     end
 
