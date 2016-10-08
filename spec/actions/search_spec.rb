05ab6b5c2967e02d5918927892ae13fbe49ee7a8
Vell <vmcilwain@me.com>
2016-10-08 14:16:29 -0400

    update output

--- spec/actions/search_spec.rb
+++ spec/actions/search_spec.rb
@@ -12,16 +12,16 @@ describe MyTodo do
       end
 
       it 'finds todo item by body' do
-        expect{MyTodo::Todo.start( %w[search nfl])}.to output("ToDos FOUND: 1\nSearch based on ransack search: body_or_detailed_status_or_tags_name_or_notes_body_cont\n\nID: 1 | Created On: 2016-10-07 | Tags: tag1 | Status:  | Complete: \nnfl\n\n").to_stdout
+        expect{MyTodo::Todo.start( %w[search nfl])}.to output("ToDos FOUND: 1\nSearch based on ransack search: body_or_detailed_status_or_tags_name_or_notes_body_cont\n\nID: 1 | Created On: 2016-10-08 | Tags: tag1 | Status:  | Complete: \nnfl\n\n").to_stdout
       end
 
       it 'finds todo items by associated tag' do
-        expect{MyTodo::Todo.start( %w[search tag1])}.to output("ToDos FOUND: 1\nSearch based on ransack search: body_or_detailed_status_or_tags_name_or_notes_body_cont\n\nID: 1 | Created On: 2016-10-07 | Tags: tag1 | Status:  | Complete: \nnfl\n\n").to_stdout
+        expect{MyTodo::Todo.start( %w[search tag1])}.to output("ToDos FOUND: 1\nSearch based on ransack search: body_or_detailed_status_or_tags_name_or_notes_body_cont\n\nID: 1 | Created On: 2016-10-08 | Tags: tag1 | Status:  | Complete: \nnfl\n\n").to_stdout
       end
 
       it 'finds todo items by detailed status' do
         @todo2.update!(detailed_status: 'None')
-        expect{MyTodo::Todo.start( %w[search None])}.to output("ToDos FOUND: 1\nSearch based on ransack search: body_or_detailed_status_or_tags_name_or_notes_body_cont\n\nID: 2 | Created On: 2016-10-07 | Tags:  | Status: None | Complete: \nrocks\n\n").to_stdout
+        expect{MyTodo::Todo.start( %w[search None])}.to output("ToDos FOUND: 1\nSearch based on ransack search: body_or_detailed_status_or_tags_name_or_notes_body_cont\n\nID: 2 | Created On: 2016-10-08 | Tags:  | Status: None | Complete: \nrocks\n\n").to_stdout
       end
     end
 
