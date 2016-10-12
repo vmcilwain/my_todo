e479be8f448c0181f81fd50559f1e88b8cd492fd
Vell <lovell.m@teachingstrategies.com>
2016-10-12 14:34:50 -0400

    add missing spec for list action

---
+++ spec/actions/list_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe MyTodo do
+  describe 'list' do
+    before do
+      @todo1 = FactoryGirl.create :item, done: true
+      @todo1 = FactoryGirl.create :item, done: false
+    end
+
+    it 'displays items with done set to true' do
+      expect{MyTodo::Todo.start(%w[list done])}.to output("ToDos FOUND: 1\n\nID: 1 | Created On: #{Date.today} | Tags:  | Status:  | Complete: true\nSome Body\n\n").to_stdout
+    end
+
+    it 'displays items with done set to false by default' do
+      expect{MyTodo::Todo.start(%w[list])}.to output("ToDos FOUND: 1\n\nID: 2 | Created On: #{Date.today} | Tags:  | Status:  | Complete: false\nSome Body\n\n").to_stdout
+    end
+
+    it 'displays all items' do
+      expect{MyTodo::Todo.start(%w[list all])}.to output("ToDos FOUND: 2\n\nID: 1 | Created On: #{Date.today} | Tags:  | Status:  | Complete: true\nSome Body\n\n\nID: 2 | Created On: #{Date.today} | Tags:  | Status:  | Complete: false\nSome Body\n\n").to_stdout
+    end
+  end
+end
