9b2b0aeae37003f3d059dcff0f8a1da51dbfbf12
Vell <lovell.m@teachingstrategies.com>
2016-12-12 08:57:05 -0500

    update specs for new template output

--- spec/actions/list_spec.rb
+++ spec/actions/list_spec.rb
@@ -8,15 +8,15 @@ describe MyTodo do
     end
 
     it 'displays items with done set to true' do
-      expect{MyTodo::Todo.start(%w[list done])}.to output("ToDos FOUND: 1\n\nID: 1 | Created On: #{Date.today} | Tags:  | Status:  | Complete: true\nSome Body\n\n").to_stdout
+      expect{MyTodo::Todo.start(%w[list done])}.to output("ToDos FOUND: 1\n\nID: 1 | Created On: #{Date.today} | Tags:  | Status:  | Complete: true\nSome Body\n").to_stdout
     end
 
     it 'displays items with done set to false by default' do
-      expect{MyTodo::Todo.start(%w[list])}.to output("ToDos FOUND: 1\n\nID: 2 | Created On: #{Date.today} | Tags:  | Status:  | Complete: false\nSome Body\n\n").to_stdout
+      expect{MyTodo::Todo.start(%w[list])}.to output("ToDos FOUND: 1\n\nID: 2 | Created On: #{Date.today} | Tags:  | Status:  | Complete: false\nSome Body\n").to_stdout
     end
 
     it 'displays all items' do
-      expect{MyTodo::Todo.start(%w[list all])}.to output("ToDos FOUND: 2\n\nID: 1 | Created On: #{Date.today} | Tags:  | Status:  | Complete: true\nSome Body\n\n\nID: 2 | Created On: #{Date.today} | Tags:  | Status:  | Complete: false\nSome Body\n\n").to_stdout
+      expect{MyTodo::Todo.start(%w[list all])}.to output("ToDos FOUND: 2\n\nID: 1 | Created On: #{Date.today} | Tags:  | Status:  | Complete: true\nSome Body\n\nID: 2 | Created On: #{Date.today} | Tags:  | Status:  | Complete: false\nSome Body\n").to_stdout
     end
   end
 end
