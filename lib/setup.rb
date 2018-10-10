6a7f5506314c3d038996739427dabb1739755c12
Vell - [I am available to join projects.] <vmcilwain@me.com>
2018-10-10 10:28:39 -0400

    update gem dir constant

--- lib/setup.rb
+++ lib/setup.rb
@@ -6,7 +6,7 @@ class Setup < Thor
   # Look into the following location for templates
   source_root "#{__dir__}/../lib/my_todo/templates"
   # Store the root of the gem directory
-  GEM_DIR = __dir__
+  GEM_DIR = "#{__dir__}/.."
   # Store users home directory
   HOME_DIR = `echo $HOME`.chomp
 
@@ -16,7 +16,7 @@ class Setup < Thor
       `mkdir -p #{HOME_DIR}/.my_todo/data`
       say "Created .my_todo in #{HOME_DIR}"
     end
-    template "config.yml.erb", "#{GEM_DIR}/lib/db/config.yml", force: true
+    template "config.yml.erb", "#{GEM_DIR}/db/config.yml", force: true
   end
 
   desc 'standard_migrations_override', 'Generate SM override file'
