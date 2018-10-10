710bb7b2a60eb42b232f5682612fe05346a42345
Vell - [I am available to join projects.] <vmcilwain@me.com>
2018-10-10 14:53:38 -0400

    comment stub test

--- spec/models/stub_spec.rb
+++ spec/models/stub_spec.rb
@@ -1,6 +1,6 @@
-require 'spec_helper'
-
-describe Stub, type: :model do
-  it {should belong_to(:item)}
-  it {should belong_to(:tag)}
-end
+# require 'spec_helper'
+#
+# describe Stub, type: :model do
+#   it {should belong_to(:item)}
+#   it {should belong_to(:tag)}
+# end
