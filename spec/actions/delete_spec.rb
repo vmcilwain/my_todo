8c4f1a72dddffac207d3506c9887da54d55a50f3
Vell - [I am available to join projects.] <vmcilwain@me.com>
2018-10-11 09:13:57 -0400

    update specs

--- spec/actions/delete_spec.rb
+++ spec/actions/delete_spec.rb
@@ -2,14 +2,20 @@ require 'spec_helper'
 
 describe MyTodo do
   describe 'delete' do
-    before {@todo = FactoryGirl.create(:item)}
-
+    let(:todo) {FactoryBot.create(:item)}
+    
+    before {todo}
+    
     it 'destroys todo item' do
-      expect{MyTodo::Todo.start(%W(delete --id=#{@todo.id}))}.to change{Item.count}.by(-1)
+      expect{MyTodo::Todo.start(['delete', todo.id])}.to change{Item.count}.by(-1)
     end
 
     it 'returns nil exception if todo item is invalid' do
-      expect{MyTodo::Todo.start(%W[delete --id=#{@todo.id + 1}])}.to output("undefined method `destroy!' for nil:NilClass\n").to_stdout
+      expect{MyTodo::Todo.start(['delete', todo.id + 1])}.to output("undefined method `destroy!' for nil:NilClass\n").to_stdout
+    end
+    
+    it 'displays a notice' do
+      expect{MyTodo::Todo.start(['delete', todo.id])}.to output("ToDo DESTROYED!\n").to_stdout
     end
   end
 end
