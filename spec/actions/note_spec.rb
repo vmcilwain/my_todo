45e4aada65e5a0b69f72fff3dde8eab443ae1e3f
Vell - [I am available to join projects.] <vmcilwain@me.com>
2018-10-11 10:09:43 -0400

    update specs

--- spec/actions/note_spec.rb
+++ spec/actions/note_spec.rb
@@ -2,25 +2,27 @@ require 'spec_helper'
 
 describe MyTodo do
   describe 'add_note' do
-    before {@todo = FactoryGirl.create(:item)}
+    let(:todo) {FactoryBot.create :item}
+    let(:body) {Faker::Lorem.words(5).join("\s")}
+    
     context 'successful creation' do
       it 'creates a note' do
-        expect{MyTodo::Todo.start(%W[note --id=#{@todo.id} --body=note_text])}.to change{Note.count}.by(1)
+        expect{MyTodo::Todo.start(['note', todo.id, body])}.to change{Note.count}.by(1)
       end
 
-      it 'associates note to item' do
-        MyTodo::Todo.start(%W[note --id=#{@todo.id} --body=note_text])
-        expect(@todo.notes.size).to eq 1
+      it 'associates note to item', 'ehh' do
+        MyTodo::Todo.start(['note', todo.id, body])
+        expect(todo.notes.size).to eq 1
       end
 
       it 'displays item with notes' do
-        expect{MyTodo::Todo.start(%W[note --id=#{@todo.id} --body=note_text])}.to output("Notes for 1: Some Body\nID: 1 | Created On: #{Date.today}\nnote_text\n\n\n").to_stdout
+        expect{MyTodo::Todo.start(['note', todo.id, body])}.to output("notes for item #{todo.id}: #{todo.body}\n\nid: 1\ncreated: #{Date.today}\n\n#{body}\n\n\n").to_stdout
       end
     end
 
     context 'unsuccessful creation' do
       it 'returns exception if partent item is not found' do
-        expect{MyTodo::Todo.start(%W[note --id=#{@todo.id + 1}])}.to output("undefined method `notes' for nil:NilClass\n").to_stdout
+        expect{MyTodo::Todo.start(['note', todo.id + 1])}.to output("undefined method `notes' for nil:NilClass\n").to_stdout
       end
     end
   end
